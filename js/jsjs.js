c6460f8b903a4cab5c8383522bb7ac38c71cbccb
duhui1998 <799408285@qq.com>
2019-06-13 16:53:50 +0800

    提交

---
+++ js/jsjs.js
@@ -0,0 +1,85 @@
+	function li(){
+		document.getElementById("img1").src="images/sc1.jpg";
+		document.getElementById("img2").src="images/sc1.jpg";
+		document.getElementById("img3").src="images/sc1.jpg";
+		document.getElementById("img4").src="images/sc1.jpg";
+	}
+	function li2(){
+		document.getElementById("img1").src="images/sc2.jpg";
+		document.getElementById("img2").src="images/sc2.jpg";
+		document.getElementById("img3").src="images/sc2.jpg";
+		document.getElementById("img4").src="images/sc2.jpg";
+	}
+	function li3(){
+		document.getElementById("img1").src="images/sc3.jpg";
+		document.getElementById("img2").src="images/sc3.jpg";
+		document.getElementById("img3").src="images/sc3.jpg";
+		document.getElementById("img4").src="images/sc3.jpg";
+	}
+	function li4(){
+		document.getElementById("img1").src="images/sc4.jpg";
+		document.getElementById("img2").src="images/sc4.jpg";
+		document.getElementById("img3").src="images/sc4.jpg";
+		document.getElementById("img4").src="images/sc4.jpg";
+	}
+	function li5(){
+		document.getElementById("img1").src="images/sc2.jpg";
+		document.getElementById("img2").src="images/sc2.jpg";
+		document.getElementById("img3").src="images/sc2.jpg";
+		document.getElementById("img4").src="images/sc2.jpg";
+	}
+	function li6(){
+		document.getElementById("img1").src="images/sc3.jpg";
+		document.getElementById("img2").src="images/sc3.jpg";
+		document.getElementById("img3").src="images/sc3.jpg";
+		document.getElementById("img4").src="images/sc3.jpg";
+	}
+	function li7(){
+		document.getElementById("img1").src="images/sc2.jpg";
+		document.getElementById("img2").src="images/sc2.jpg";
+		document.getElementById("img3").src="images/sc2.jpg";
+		document.getElementById("img4").src="images/sc2.jpg";
+	}
+	function li8(){
+		document.getElementById("img1").src="images/sc4.jpg";
+		document.getElementById("img2").src="images/sc4.jpg";
+		document.getElementById("img3").src="images/sc4.jpg";
+		document.getElementById("img4").src="images/sc4.jpg";
+	}
+	function li9(){
+		document.getElementById("img1").src="images/sc2.jpg";
+		document.getElementById("img2").src="images/sc2.jpg";
+		document.getElementById("img3").src="images/sc2.jpg";
+		document.getElementById("img4").src="images/sc2.jpg";
+	}
+	function li10(){
+		document.getElementById("img1").src="images/sc1.jpg";
+		document.getElementById("img2").src="images/sc1.jpg";
+		document.getElementById("img3").src="images/sc1.jpg";
+		document.getElementById("img4").src="images/sc1.jpg";
+	}
+	function li11(){
+		document.getElementById("img1").src="images/sc2.jpg";
+		document.getElementById("img2").src="images/sc2.jpg";
+		document.getElementById("img3").src="images/sc2.jpg";
+		document.getElementById("img4").src="images/sc2.jpg";
+	}
+	function li12(){
+		document.getElementById("img1").src="images/sc1.jpg";
+		document.getElementById("img2").src="images/sc1.jpg";
+		document.getElementById("img3").src="images/sc1.jpg";
+		document.getElementById("img4").src="images/sc1.jpg";
+	}
+	function li13(){
+		document.getElementById("img1").src="images/sc2.jpg";
+		document.getElementById("img2").src="images/sc2.jpg";
+		document.getElementById("img3").src="images/sc2.jpg";
+		document.getElementById("img4").src="images/sc2.jpg";
+	}
+	function li14(){
+		document.getElementById("img1").src="images/sc3.jpg";
+		document.getElementById("img2").src="images/sc3.jpg";
+		document.getElementById("img3").src="images/sc3.jpg";
+		document.getElementById("img4").src="images/sc3.jpg";
+	}
+// JavaScript Document
